854f317def76eaa48d883e8860c7a738b9489999
Lance Newman <lancelot316@msn.com>
2017-04-10 20:07:28 -0400

    created list rentals acceptance tests.

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,28 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should show rentals as the home page', function(assert){
+
+});
+
+test('should link to the information about the company', function(assert){
+
+});
+
+test('should link to contact information', function(assert){
+
+});
+
+test('should list available rentals', function(assert){
+
+});
+
+test('should filter the list of rentals bt city', function(assert){
+
+});
+
+test('should show details of a selected rental', function(assert){
+
+});
